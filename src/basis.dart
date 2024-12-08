7dcbe92d14f4d7af54e7e785c4d3d76eb947a355
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-08 14:18:16 +0300

    toplama işlemi örnegi

--- src/basis.dart
+++ src/basis.dart
@@ -1,7 +1,18 @@
 void main() {
-  var firstName = "Emre";
-  var lastName = "SEFEROĞLU";
-  var age = 16;
+  String firstName = "Emre";
+  String lastName = "SEFEROĞLU";
+  int age = 16;
+  bool isActive = true;
+  var users = ["Emre", "Ahmet", "Mehmet"];
 
   print(firstName + " " + lastName + " kişisi " + age.toString() + " yaşında");
+  print("Çalışıyormu? " + isActive.toString());
+  print("--------------");
+  users.map((user) => print("Kullanıcı:" + user));
+
+  const int num1 = 12;
+  const int num2 = 15;
+  const int result = num1 + num2;
+
+  print("Sonuç: " + result.toString());
 }

3fb951b9d278406740d2ca4bff231d7e15799e47
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-08 14:23:57 +0300

    kullanıcıdan 2 tane girdi alıp sayı toplama işlemi

--- src/basis.dart
+++ src/basis.dart
@@ -1,18 +1,33 @@
+import 'dart:io';
+
 void main() {
-  String firstName = "Emre";
-  String lastName = "SEFEROĞLU";
-  int age = 16;
-  bool isActive = true;
-  var users = ["Emre", "Ahmet", "Mehmet"];
+  // String firstName = "Emre";
+  // String lastName = "SEFEROĞLU";
+  // int age = 16;
+  // bool isActive = true;
+  // var users = ["Emre", "Ahmet", "Mehmet"];
 
-  print(firstName + " " + lastName + " kişisi " + age.toString() + " yaşında");
-  print("Çalışıyormu? " + isActive.toString());
-  print("--------------");
-  users.map((user) => print("Kullanıcı:" + user));
+  // print(firstName + " " + lastName + " kişisi " + age.toString() + " yaşında");
+  // print("Çalışıyormu? " + isActive.toString());
+  // print("--------------");
+  // users.map((user) => print("Kullanıcı:" + user));
 
-  const int num1 = 12;
-  const int num2 = 15;
-  const int result = num1 + num2;
+  // const int num1 = 12;
+  // const int num2 = 15;
+  // const int result = num1 + num2;
 
-  print("Sonuç: " + result.toString());
+  // print("Sonuç: " + result.toString());
+
+  // print("1.Sayıyı giriniz");
+  // int num = stdin.readByteSync();
+  // print("2.Sayıyı giriniz");
+  // int num2 = stdin.readByteSync();
+  // print("Girilen 2 sayının toplamı :" + num.toString() + num2.toString());
+
+  print("Enter Two Number:");
+  String? num1 = stdin.readLineSync();
+  String? num2 = stdin.readLineSync();
+
+  print(
+      "The entered name is ${int.parse(num1.toString()) + int.parse(num2.toString())}");
 }
